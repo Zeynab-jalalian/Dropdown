2d22f2532d208c80f43165ecacda7a325303a613
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-11 11:36:47 +0330

    finish

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ let dropdown=document.querySelector('.dropdown');
 dropdown.onclick=function(){
   dropdown.classList.toggle('active');
 }
+function show(text){
+document.querySelector('.dropdown-text').value=text;
+}
